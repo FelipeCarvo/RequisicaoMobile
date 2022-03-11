d3a54b93f85b59dc5ea7f883dc27dbcb73fd3b99
carlos_octavio <ta.tavinho@hotmail.com>
2022-03-11 09:11:52 -0300

    ajustes

--- src/app/shared/components/menu-cp/menu.component.ts
+++ src/app/shared/components/menu-cp/menu.component.ts
@@ -1,28 +1,33 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import {Logout} from '@core/store/actions/auth.actions'
+import { ResetStateInsumos } from '@core/store/actions/insumos.actions';
+import {ResetStateReq} from '@core/store/actions/req.actions'
+import { Store } from '@ngxs/store';
 @Component({
   selector: 'menu-root',
   templateUrl: 'menu.component.html',
   styleUrls: ['menu.component.scss'],
 })
 export class MenuComponent {
-  constructor(private menu: MenuController,private router:Router) {}
+  constructor(private menu: MenuController,private router:Router,private store:Store) {}
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
   }
-
-  openEnd() {
-    this.menu.open('end');
-  }
-
-  openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
-  }
   redirect(route){
     this.router.navigate([ `/tabs/${route}`]);
     this.menu.close()
+  }
+  exit(){
+    this.menu.close()
+    setTimeout(() =>{
+      this.store.dispatch(new ResetStateReq())
+      this.store.dispatch(new ResetStateInsumos());
+      this.store.dispatch(new Logout())
+      this.router.navigate([ `/tabs/login`]);
+    },200)
+
   }
 }
