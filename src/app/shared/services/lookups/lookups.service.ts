c8f435feaeb7e275a57f8509792a2726ac6756c3
carlos_octavio <ta.tavinho@hotmail.com>
2022-08-15 16:12:04 -0300

    lastCommit

--- src/app/shared/services/lookups/lookups.service.ts
+++ src/app/shared/services/lookups/lookups.service.ts
@@ -3,19 +3,24 @@ import { Injectable } from '@angular/core';
 import {environment} from '@environment/environment';
 import { Store } from '@ngxs/store';
 import {LookupsEndPoints} from '../utils/enums/EnumLockup'
-
-@Injectable({
+import { AuthUser } from '@core/store/state/auth.state';
+  @Injectable({
     providedIn: 'root'
   })
   export class LoockupstService {
     sieconwebwebapi = `${environment.sieconwebwebapi}`;
-
-    constructor(private http:HttpClient, private store:Store,){}
+    constructor(private http:HttpClient, private store:Store,){
+      // this.sieconwebsuprimentos = this.getUrlParams.urlAPISuprimentos;
+      this.sieconwebwebapi = this.getUrlParams.urlAPISP7;
+    }
     filterLockup(el){
       el = el.split(" ")[0];
       el = el.replace(/[^0-9]/g,'');
       return parseInt(el);
     }
+    public get getUrlParams(){
+      return this.store.selectSnapshot(AuthUser.geturlParams)
+    }  
     getLookUp(params = null, endPoint = 'Empreendimentos'){
       return new Promise((resolve, reject) => {
      
